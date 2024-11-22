1849038d31e446a5c85034da4875273a4181bc9b
Nvlahogiannis12 <526Nvlahogiannis@frhsd.com>
2024-11-22 15:55:38 +0000

    Cromebook Commit

--- script.js
+++ script.js
@@ -42,7 +42,9 @@ let artifact3 = prompt('Name a noun')
 let monster = prompt('Name a mythical monster')
 let monster2 = prompt('Name a animal or monster')
 
-let myStoryOne = ` <p> A long long time ago a ${userName1} cast a dark spell on the kingdom of ${placeName1} . A dark cloud ${adjective1} into the kingdom. The ${userName1} wished to ${wish1} . The dark cloud helped them succeed. But a great knight wielding a ${weapon1} and a ${weapon2} . In order to defeat the ${userName1} the great knight had to travel to the ${biome1}, ${biome2}, and the ${biome3}. First the knight traveled to the ${biome1}, the darkness had already descended on the land but the knight ran in with his ${weapon1} and managed to get the magical artifact, the ${artifact1}. Next they traveled to the ${biome2}, this place was completely untouched with a large ${building} for defense. In order to get the ${artifact2} the knight had to ${verb1}. The final piece was a ${artifact3} in the ${biome3}. The ${artifact3} was guarded by a ${monster} the knight fought off the ${monster} with their ${weapon2}. After getting back to ${placeName1}. They found the ${userName1} had completely taken over the kingdom. Using the power of the artifacts the knight decided they must turn into a ${monster2} and seal away the ${userName1} bringing peace to ${placeName1}. </p>`
+let myStoryOne = `<h2>The Legend</h2>
+
+<p> A long long time ago a ${userName1} cast a dark spell on the kingdom of ${placeName1} . A dark cloud ${adjective1} into the kingdom. The ${userName1} wished to ${wish1} . The dark cloud helped them succeed. But a great knight wielding a ${weapon1} and a ${weapon2} . In order to defeat the ${userName1} the great knight had to travel to the ${biome1}, ${biome2}, and the ${biome3}. First the knight traveled to the ${biome1}, the darkness had already descended on the land but the knight ran in with his ${weapon1} and managed to get the magical artifact, the ${artifact1}. Next they traveled to the ${biome2}, this place was completely untouched with a large ${building} for defense. In order to get the ${artifact2} the knight had to ${verb1}. The final piece was a ${artifact3} in the ${biome3}. The ${artifact3} was guarded by a ${monster} the knight fought off the ${monster} with their ${weapon2}. After getting back to ${placeName1}. They found the ${userName1} had completely taken over the kingdom. Using the power of the artifacts the knight decided they must turn into a ${monster2} and seal away the ${userName1} bringing peace to ${placeName1}. </p>`
 
 console.log(myStoryOne)
 
@@ -65,7 +67,9 @@ let onomatopoeia = prompt('Name an Onomatopoeia')
 let family = prompt('Name a family member (aunt, cousin, etc.)')
 let noun3 = prompt('Name a noun')
 
- let myStoryTwo = `<p> On the distant planet of ${planetName}, A somewhat long time ago in a universe far from our own. A man by the name of ${userName2} was just an ${adjective2} man. He was a ${occupation1}, and was good at his job. One day ${userName2} returns to his home to find an old man at his ${room}. The man was ${adjective3} he told a tale of an age long past. Of a wizard who could control and manipulate ${noun2}. The old man told of how the wizard grew ${adjective4} and that led to their downfall of turning to darkness. A ${onomatopoeia} was heard in the distance the old man held out his hand and told ${userName2} that wasn't a story. It was the tale of what happened to his ${family}. The old man explained that he was once their teacher but failed. The old man had ${noun3} in his hand. He said this was once your ${family}’s. Now you must bring balance to the magic so will you take the ${noun3}? </p>`
+ let myStoryTwo = `<h2>A long long time ago...</h2>
+ 
+ <p> On the distant planet of ${planetName}, A somewhat long time ago in a universe far from our own. A man by the name of ${userName2} was just an ${adjective2} man. He was a ${occupation1}, and was good at his job. One day ${userName2} returns to his home to find an old man at his ${room}. The man was ${adjective3} he told a tale of an age long past. Of a wizard who could control and manipulate ${noun2}. The old man told of how the wizard grew ${adjective4} and that led to their downfall of turning to darkness. A ${onomatopoeia} was heard in the distance the old man held out his hand and told ${userName2} that wasn't a story. It was the tale of what happened to his ${family}. The old man explained that he was once their teacher but failed. The old man had ${noun3} in his hand. He said this was once your ${family}’s. Now you must bring balance to the magic so will you take the ${noun3}? </p>`
 
  console.log(myStoryTwo)
 
@@ -88,7 +92,9 @@ let number1 = prompt('Name a Number')
 let number2 = prompt('Name a Number')
 let number3 = prompt('Name a Number')
 
-let myStoryThree = `Do you or someone you love ${problem}. Well, do we have the solution for you. Introducing ${productName} the ${adjective5} new product designed to solve ${problem}. Ever wonder how it works? Well here's your answer. The ${productName} works to remove your problem ${adverb}. If you worry this product will make ${noun4} well not to worry the ${productName} is absolutely free allowing for a ${adjective6} environment for your pets and family. But wait there's more if you are also tired of ${problem2} the ${productName} also takes care of that with no additional fee. To order yours today call ${phoneNumber} if you order in the next ${number1} hours you will receive a free instruction guide. That's quite the deal only after ${number2} payments of ${number3} dollars. Order Now!`
+let myStoryThree = `<h2>Advertisment</h2>
+
+<p>Do you or someone you love ${problem}. Well, do we have the solution for you. Introducing ${productName} the ${adjective5} new product designed to solve ${problem}. Ever wonder how it works? Well here's your answer. The ${productName} works to remove your problem ${adverb}. If you worry this product will make ${noun4} well not to worry the ${productName} is absolutely free allowing for a ${adjective6} environment for your pets and family. But wait there's more if you are also tired of ${problem2} the ${productName} also takes care of that with no additional fee. To order yours today call ${phoneNumber} if you order in the next ${number1} hours you will receive a free instruction guide. That's quite the deal only after ${number2} payments of ${number3} dollars. Order Now! </p>`
 
 console.log(myStoryThree)
 
@@ -115,7 +121,9 @@ let monster3 = prompt('Name a Monster')
 let adverb2 = prompt('Name an adverb')
 let date = prompt('Whats todays date')
 
-let myStoryFour = `Your car breaks down on a ${adjective7} road. ${adverb1} your car stops directly in front of an old ${adjective8} mansion. You knock on the door to ask for ${noun5}. A man in a ${clothes} opens the door and ${verb2} at you before telling you to enter. You stay in the main lobby with a large ${noun6} hangs from the ceiling cobwebs and dust everywhere. it would appear no one has been there in ${number4} years. Suddenly you hear ${onomatopoeia1} the windows ${verb3} open and you hear voices and whispers. A ${noun7} and a ${noun8} fly past you with no one nearby. Suddenly the ${noun5} crashes to the floor and a ${monster3} enters ${adverb2} into the room. It was ${date}, your birthday.`
+let myStoryFour = `<h2>Spooky Manor</h2>
+
+<p>Your car breaks down on a ${adjective7} road. ${adverb1} your car stops directly in front of an old ${adjective8} mansion. You knock on the door to ask for ${noun5}. A man in a ${clothes} opens the door and ${verb2} at you before telling you to enter. You stay in the main lobby with a large ${noun6} hangs from the ceiling cobwebs and dust everywhere. it would appear no one has been there in ${number4} years. Suddenly you hear ${onomatopoeia1} the windows ${verb3} open and you hear voices and whispers. A ${noun7} and a ${noun8} fly past you with no one nearby. Suddenly the ${noun5} crashes to the floor and a ${monster3} enters ${adverb2} into the room. It was ${date}, your birthday.</p>`
 
 console.log(myStoryFour)
 
